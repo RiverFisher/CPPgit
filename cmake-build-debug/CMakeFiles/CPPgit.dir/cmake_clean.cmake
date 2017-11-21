5ea0bddaac8aba72a973c0b4ec6a2dca276079a8
wizard <goldenarius@gmail.com>
2017-11-22 07:26:16 +1000

    New project was created.

---
+++ cmake-build-debug/CMakeFiles/CPPgit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPgit.dir/main.cpp.o"
+  "CPPgit.pdb"
+  "CPPgit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPgit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
