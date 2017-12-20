1c9d4ab8e6f6700197101ab07b3ecb93d848e44d
Alexander <colddegree@yandex.ru>
2017-12-20 17:06:40 +1000

    Task8 and Task9 headers were added

--- Alexander_Melnik/master.cpp
+++ Alexander_Melnik/master.cpp
@@ -1,6 +1,9 @@
 #include "master.h"
 #include <iostream>
 
+#include "Task8/builder.h"
+#include "Task9/builder.h"
+
 int thread1() {
     std::cout << "Thread 1 was run." << std::endl;
     std::cout << "Alexander Melnik" << std::endl;
