4c6a7025c1065e4ecc9e630e2da78444030984e1
Alexander <colddegree@yandex.ru>
2017-12-20 18:20:26 +1000

    Task8 init procedure was wrapped in namespace

--- Dmitry_Fedorenko/master.cpp
+++ Dmitry_Fedorenko/master.cpp
@@ -1,8 +1,12 @@
 #include "master.h"
 #include <iostream>
 
+#include "Task8/conventer.h"
+
 int thread3() {
     std::cout << "Thread 3 was run." << std::endl;
+    std::cout << "Dmitry Fedorenko" << std::endl;
+    Dmitry_Fedorenko::initTask8();
 
     return 0;
 }
