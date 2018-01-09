0509e37b247080b66040025de440c389e4c38621
Marina2927 <32886882+Marina2927@users.noreply.github.com>
2018-01-09 20:32:16 +1000

    Add files via upload

---
+++ Marina_Gribacheva/task_8/tests.cpp
@@ -0,0 +1,17 @@
+#include "tests.h"
+#include <string>
+#include <iostream>
+
+bool test_1(std::string str){
+    if (str == "g4r5w3q1o2"){
+        return true;
+    }
+    return false;
+}
+
+bool test_2(std::string str){
+    if (str == "ggggrrrrrwwwqoo"){
+        return true;
+    }
+    return false;
+}
