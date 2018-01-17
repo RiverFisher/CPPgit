7dd4fc38ec2ace66274697f170a645d173a67da5
Acrom7 <yadima19988@gmail.com>
2018-01-17 10:59:45 +1000

    Marina

---
+++ Marina_Gribacheva/17_01_2018/Task_8/tests.h
@@ -0,0 +1,10 @@
+#ifndef NEWPROJECT_TESTS_H
+#define NEWPROJECT_TESTS_H
+
+#include <string>
+#include <iostream>
+
+bool test_1(std::string);
+bool test_2(std::string);
+
+#endif //NEWPROJECT_TESTS_H
